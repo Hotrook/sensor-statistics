1ab3ab19540331d5957db79a61cb621059cb5872
Hotrook <mrozsebastian@wp.pl>
2019-03-30 10:44:26 +0100

    add basic project structure

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "sensor-statistics"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
---
+++ src/main/scala/com/hotrook/App.scala
@@ -0,0 +1,7 @@
+package com.hotrook
+
+object App {
+  def main(args: Array[String]): Unit = {
+    println("Let's start!")
+  }
+}
