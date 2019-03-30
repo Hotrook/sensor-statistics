3f2bc4fa43035bf499d8ce7b2322690e6a25ef32
Hotrook <mrozsebastian@wp.pl>
2019-03-30 22:51:48 +0100

    reformat code

--- src/main/scala/com/hotrook/actors/FileProcessor.scala
+++ src/main/scala/com/hotrook/actors/FileProcessor.scala
@@ -7,12 +7,14 @@ import akka.actor.{Actor, ActorLogging, Props}
 object FileProcessor {
   def props: Props = Props(new FileProcessor)
 
-  case class LoadFile(file : File)
+  case class LoadFile(file: File)
 
-  case class FileLoaded(files : String)
+  case class FileLoaded(files: String)
+
 }
 
 class FileProcessor extends Actor with ActorLogging {
+
   import FileProcessor._
 
   override def receive = {
