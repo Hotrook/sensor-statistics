daad6d248b7b233f9a3edff026688d359883e49e
Hotrook <mrozsebastian@wp.pl>
2019-03-31 18:56:07 +0200

    add SensorManager actor

---
+++ src/main/scala/com/hotrook/actors/SensorManager.scala
@@ -0,0 +1,30 @@
+package com.hotrook.actors
+
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+
+import scala.collection.mutable.Map
+
+object SensorManager {
+  def props: Props = Props(new SensorManager)
+}
+
+class SensorManager extends Actor with ActorLogging {
+
+  val sensorIdToActor = Map.empty[String, ActorRef]
+
+  override def receive: Receive = {
+    case trackMsg@SensorDataStreamer.SensorData(sensorId, _) =>
+      sensorIdToActor.get(sensorId) match {
+        case Some(sensor) =>
+          sensor forward trackMsg
+        case None =>
+          val newSensor = context.actorOf(Sensor.props(sensorId), s"sensor-${sensorId}")
+          sensorIdToActor += sensorId -> newSensor
+          newSensor forward trackMsg
+      }
+    case SensorDataStreamer.FinishProcessing =>
+      sensorIdToActor.foreach {
+        case (_, sensor) => sensor forward SensorDataStreamer.FinishProcessing
+      }
+  }
+}
